d532a33be721f91c6e1faa53e70c376f620d3c17
Anjan <gh.anjangh@gmail.com>
2024-09-07 20:23:38 +0530

    updated renv_base

--- scripts/renv_base.R
+++ scripts/renv_base.R
@@ -4,3 +4,21 @@ renv::init()
 renv::snapshot()
 
 renv::restore()
+
+renv::update()
+
+renv::history()
+
+renv::revert()
+
+renv::deactivate()
+renv::deactivate(clean = TRUE)
+
+renv::activate()
+
+# If you want to stop using renv for all your projects, 
+# you’ll also want to remove renv's global infrastructure 
+# with the following R code
+root <- renv::paths$root()
+unlink(root, recursive = TRUE)
+utils::remove.packages("renv")
