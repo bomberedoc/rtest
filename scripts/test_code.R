5d3c6eca57419a1403f0040e71f7ad6fa72524fb
Anjan <gh.anjangh@gmail.com>
2025-01-14 12:42:06 +0530

    added a custom pal

--- scripts/test_code.R
+++ scripts/test_code.R
@@ -17,3 +17,9 @@ library(pal)
 
 options(.pal_fn = "chat_ollama",
         .pal_args = list(model = "llama3.2:1b"))
+
+prompt_new(
+  "quartochunk", 
+  "replace", 
+  contents = "https://gist.githubusercontent.com/hfrick/1ca8fc2cb2a4409b743e8120c6cc2223/raw/a9703edfbd4e83839af0278c33add1b33e243d02/quartochunk-replace.md"
+)
